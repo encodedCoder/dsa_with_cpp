48b1f5686bcf71b610cebc413623046d341bff00
shhotu010 <sureshrao100000@gmail.com>
2022-11-09 18:36:15 +0530

    Add MinHeap and MaxHeap Programs to DSA

---
+++ 04_DSA/07_heap/02_minHeap.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+class MinHeap{
+private:
+    int length;
+    vector<int> heap;
+public:
+    MinHeap(){
+        this->length = 0;
+    }
+    void push(int val){
+        if (this->length == 0){
+            this->heap.push_back(val);
+            this->length++;
+            return;
+        }
+
+        this->heap.push_back(val);
+        this->length++;
+        int child = this->length-1;
+        while (child > 0){
+            int parent = ceil(child/2.0)-1;
+            // cout << "Parent: " << parent <<"," << "child: " << child << endl; 
+            if (this->heap[parent] > this->heap[child]) {
+                swap(this->heap[parent], this->heap[child]);
+                child = parent;
+            } else {
+                break;
+            }
+        }
+    }
+    void pop(){
+        if (this->length == 0) {
+            return;
+        } else if (this->length == 1) {
+            this->length--;
+            return;
+        } else {
+            swap(this->heap[0], this->heap[this->length - 1]);
+            this->length--;
+            min_heapify(0);
+        }
+    }
+    int top(){
+        return this->heap.front();
+    }
+    void print(){
+        for(int i=0; i < this->length; ++i) {
+            cout << this->heap[i] << " ";
+        } cout << endl;
+    }
+    void min_heapify(int index) {
+        int smallest = index;
+        int left     = 2*index + 1;
+        int right    = 2*index + 2;
+
+        if (left < this->length and this->heap[smallest] > this->heap[left]) {
+            smallest = left;
+        }
+
+        if (right < this->length and this->heap[smallest] > this->heap[right]) {
+            smallest = right;
+        }
+
+        if (index != smallest) {
+            swap(this->heap[index], this->heap[smallest]);
+            min_heapify(smallest);
+        } 
+
+        return;
+    }
+    bool empty(){
+        return this->length == 0;
+    }
+    int size(){
+        return this->length;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    MinHeap max_heap;
+    max_heap.push(5);
+    max_heap.push(1);
+    max_heap.push(8);
+    max_heap.push(11);
+    max_heap.push(2);
+    max_heap.push(4);
+    max_heap.push(9);
+
+    max_heap.print();
+
+    max_heap.pop();
+    max_heap.print();
+
+    cout << "Top: " <<max_heap.top() << endl;
+    cout << "Size: " <<max_heap.size() << endl;
+
+    while (not max_heap.empty()) {
+        cout << max_heap.top() << " ";
+        max_heap.pop();
+    } cout << endl;
+
+    return 0;
+}
