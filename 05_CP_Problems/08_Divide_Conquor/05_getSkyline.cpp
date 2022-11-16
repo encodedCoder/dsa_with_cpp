efa140b0d38e572e21963103e7d9c10b7bfb8205
shhotu010 <sureshrao100000@gmail.com>
2022-11-16 08:35:22 +0530

    Add GetSkyLine to Divide and Conquer Problems

---
+++ 05_CP_Problems/08_Divide_Conquor/05_getSkyline.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+        
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
