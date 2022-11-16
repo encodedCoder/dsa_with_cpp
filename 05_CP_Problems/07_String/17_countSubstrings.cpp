e6cea7e7adbdabdf1399b1722884bf365f278fda
shhotu010 <sureshrao100000@gmail.com>
2022-11-16 08:24:38 +0530

    Add CountSubstrings to String Problems

---
+++ 05_CP_Problems/07_String/17_countSubstrings.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+        
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+	string s = "abc";
+	// string s = "aaa";
+
+	Solution obj;
+	cout << obj.countSubstrings(s) << endl;
+
+	return 0;
+}
