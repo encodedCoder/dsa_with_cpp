589ec1974f8599702e6d488b4acc59f7b37e3677
shhotu010 <sureshrao100000@gmail.com>
2022-11-17 08:08:03 +0530

    Add CanMeasureWater to Greedy Algo Problems

---
+++ 05_CP_Problems/09_Greedy/05_canMeasureWater.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
+		
+	}
+};
+
+
+int main(){
+	
+	return 1;
+}
+
+
+
