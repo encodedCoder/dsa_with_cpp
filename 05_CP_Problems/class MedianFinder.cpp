3bcb4395e8377960afd0d12e5bb484b3387a7306
shhotu010 <sureshrao100000@gmail.com>
2022-11-18 08:07:56 +0530

    Add MedianFinder Class to CP_Problems

---
+++ 05_CP_Problems/class MedianFinder.cpp
@@ -0,0 +1,20 @@
+class MedianFinder {
+    vector<int> store; // resize-able container
+
+public:
+    // Adds a number into the data structure.
+    void addNum(int num)
+    {
+        if (store.empty())
+            store.push_back(num);
+        else
+            store.insert(lower_bound(store.begin(), store.end(), num), num);     // binary search and insertion combined
+    }
+
+    // Returns the median of current data stream
+    double findMedian()
+    {
+        int n = store.size();
+        return n & 1 ? store[n / 2] : ((double) store[n / 2 - 1] + store[n / 2]) * 0.5;
+    }
+};
